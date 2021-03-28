fdeb9a13dafde1109ab6c9acee1bcdc487f08a6d
Man Chai Jun <manchaijun@gmail.com>
2021-03-29 02:59:08 +0800

    remarks

--- manageAppCheck.php
+++ manageAppCheck.php
@@ -2,8 +2,11 @@
 include_once 'database.php';
 session_start();
 $status = $_POST['status'];
+$remarks = $_POST['remarks'];
 print_r($status);
-$sql = "SELECT * FROM `application`";
+$sql = "SELECT * FROM application";
+$sql = "UPDATE application SET status= '$status', remarks= '$remarks'
+        WHERE applicationid = $appid";
 $_SESSION['appList']= [];
 
 $result = $connection->query($sql);
